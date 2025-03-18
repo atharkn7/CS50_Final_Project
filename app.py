48512ec78e0484f84a5891ded2bfdedbd80570f0
atharkn7 <athar.kn7@gmail.com>
2025-03-18 03:55:16 -0400

    linking to cs50

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from cs50 import SQL
 from flask import Flask, render_template
 
 app = Flask(__name__) 
 
+# Linking to DB
+db = SQL("sqlite:///health.db")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    users = db.execute("SELECT * FROM users")
+    return render_template('index.html', users=users)
